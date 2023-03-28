03598eb4d8d89aa9ec080ed568724fae0657d977
nodeFade <Fadelb@axis.com>
2023-03-28 15:14:17 +0200

    eeee

--- experimentScript.sh
+++ experimentScript.sh
@@ -24,4 +24,4 @@
 #     echo $i
 # done
 ok=$(curl -s --noproxy '*' --anyauth -u root:"pass" -k --header "Content-Type: application/json" --request POST --data '{"apiVersion":"1.0", "method":"getAllProperties"}' http://172.25.65.98/axis-cgi/basicdeviceinfo.cgi)
-          echo  "response from curl= ${ok}" 
+          echo  "response from curl= $ok" 
