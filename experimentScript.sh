1700c1c578245a654e907c938dd37d3804b0313d
nodeFade <Fadelb@axis.com>
2023-03-28 14:10:56 +0200

    add counter

--- experimentScript.sh
+++ experimentScript.sh
@@ -4,16 +4,22 @@
 # export ARCH=aarch64
 
 # docker build --tag $APP_NAME --build-arg $ARCH .
-DEVICE_IP=172.25.65.98
-DEVICE_PASSWORD='pass'
-ARCH=aarch64
+# DEVICE_IP=172.25.65.98
+# DEVICE_PASSWORD='pass'
+# ARCH=aarch64
 
-curl -s --anyauth -u root:"pass" "http://172.25.65.98/axis-cgi/param.cgi?action=update&root.Network.SSH.Enabled=yes" 
-ssh root@172.25.65.98 'command -v containerd >/dev/null 2>&1 && echo Compatible with Docker ACAP || echo Not compatible with Docker ACAP' 
+# curl -s --anyauth -u root:"pass" "http://172.25.65.98/axis-cgi/param.cgi?action=update&root.Network.SSH.Enabled=yes" 
+# ssh root@172.25.65.98 'command -v containerd >/dev/null 2>&1 && echo Compatible with Docker ACAP || echo Not compatible with Docker ACAP' 
 
-docker run --rm axisecp/docker-acap:latest-$ARCH $DEVICE_IP $DEVICE_PASSWORD install
-curl -s --anyauth -u "root:$DEVICE_PASSWORD" "http://$DEVICE_IP/axis-cgi/param.cgi?action=update&root.dockerdwrapper.UseTLS=yes"
+# docker run --rm axisecp/docker-acap:latest-$ARCH $DEVICE_IP $DEVICE_PASSWORD install
+# curl -s --anyauth -u "root:$DEVICE_PASSWORD" "http://$DEVICE_IP/axis-cgi/param.cgi?action=update&root.dockerdwrapper.UseTLS=yes"
 
 
-cd hello-world/tls-certs
-scp ca.pem server-cert.pem server-key.pem root@$DEVICE_IP:/usr/local/packages/dockerdwrapper/
+# cd hello-world/tls-certs
+# scp ca.pem server-cert.pem server-key.pem root@$DEVICE_IP:/usr/local/packages/dockerdwrapper/
+
+
+for i in {1..10}; do
+    sleep 1
+    echo $i
+done
