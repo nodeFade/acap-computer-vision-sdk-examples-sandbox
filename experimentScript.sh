0362b0e0fa6ce61392211425a74503d7626d0b2f
nodeFade <Fadelb@axis.com>
2023-03-29 11:23:49 +0200

    fixing the tls-setup

--- experimentScript.sh
+++ experimentScript.sh
@@ -18,13 +18,5 @@
 # cd hello-world/tls-certs
 # scp ca.pem server-cert.pem server-key.pem root@$DEVICE_IP:/usr/local/packages/dockerdwrapper/
 
-
-# for i in {1..10}; do
-#     sleep 1
-#     echo $i
-# done
-ok=$(curl -s --noproxy '*' --anyauth -u root:"pass" -k --header "Content-Type: application/json" --request POST --data '{"apiVersion":"1.0", "method":"getAllProperties"}' http://172.25.65.98/axis-cgi/basicdeviceinfo.cgi)
-echo  "response from curl= $ok" 
-ok=${ok#*Architecture}
-echo "test=$(echo $ok | cut -d '"' -f 3)" >> $GITHUB_ENV
-echo ok=$(echo $ok | cut -d '"' -f 3)
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ca.pem server-cert.pem server-key.pem root@172.25.65.98:/usr/local/packages/dockerdwrapper/
+echo "pass"
