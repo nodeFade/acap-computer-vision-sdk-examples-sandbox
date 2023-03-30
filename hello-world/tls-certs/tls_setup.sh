aefbe83f42e2f4d6306246d63231c33f59c00ba4
nodeFade <Fadelb@axis.com>
2023-03-30 12:50:21 +0200

    ls dockerdwrapper

--- hello-world/tls-certs/tls_setup.sh
+++ hello-world/tls-certs/tls_setup.sh
@@ -60,5 +60,5 @@ echo "Step 9: Copy certificate to DUT"
 sudo apt-get install sshpass
 ls
 sshpass -v -p $PASS scp -v -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ca.pem server-cert.pem server-key.pem root@$HOST:/usr/local/packages/dockerdwrapper
-sshpass -v -y -p $PASS ssh root@$HOST
+sshpass -v -o StrictHostKeyChecking=no -p $PASS ssh root@$HOST <<< "yes"
 ls /usr/local/packages/dockerdwrapper
