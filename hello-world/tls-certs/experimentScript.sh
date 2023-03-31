bfe390f96c59ffd23cac9fb8e01eb43e31da6cdd
nodeFade <Fadelb@axis.com>
2023-03-31 13:17:04 +0200

    add loop for testing

--- hello-world/tls-certs/experimentScript.sh
+++ hello-world/tls-certs/experimentScript.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
 
-# export APP_NAME=hello-world
-# export ARCH=aarch64
+# ok=$(curl -s --noproxy '*' --anyauth -u root:"pass" -k --header "Content-Type: application/json" --request POST --data '{"apiVersion":"1.0","method":"getProperties", "params": {"propertyList": ["Architecture"]}}' http://172.25.70.46/axis-cgi/basicdeviceinfo.cgi)
+# echo  "response from curl= $ok" 
+# ok=${ok#*Architecture}
+# echo "test=$(echo $ok | cut -d '"' -f 3)" >> $GITHUB_ENV
+# echo ok=$(echo $ok | cut -d '"' -f 3)
 
-# docker build --tag $APP_NAME --build-arg $ARCH .
-# DEVICE_IP=172.25.65.98
-# DEVICE_PASSWORD='pass'
-# ARCH=aarch64
-
-# curl -s --anyauth -u root:"pass" "http://172.25.65.98/axis-cgi/param.cgi?action=update&root.Network.SSH.Enabled=yes" 
-# ssh root@172.25.65.98 'command -v containerd >/dev/null 2>&1 && echo Compatible with Docker ACAP || echo Not compatible with Docker ACAP' 
-
-# docker run --rm axisecp/docker-acap:latest-$ARCH $DEVICE_IP $DEVICE_PASSWORD install
-# curl -s --anyauth -u "root:$DEVICE_PASSWORD" "http://$DEVICE_IP/axis-cgi/param.cgi?action=update&root.dockerdwrapper.UseTLS=yes"
+# ok=$(curl -s --noproxy '*' --anyauth -u root:"pass" -k --header "Content-Type: application/json" --request POST --data '{"apiVersion":"1.0","method":"getProperties", "params": {"propertyList": ["Architecture"]}}' http://172.25.65.98/axis-cgi/basicdeviceinfo.cgi)
+# echo  "response from curl= $ok" 
+# ok=${ok#*Architecture}
+# echo "test=$(echo $ok | cut -d '"' -f 3)" >> $GITHUB_ENV
+# echo ok=$(echo $ok | cut -d '"' -f 3)
 
 
-# cd hello-world/tls-certs
-# scp ca.pem server-cert.pem server-key.pem root@$DEVICE_IP:/usr/local/packages/dockerdwrapper/
-
-# sshpass -p 'pass' scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ca.pem server-cert.pem server-key.pem root@172.25.65.98:/usr/local/packages/dockerdwrapper/
-ok=$(curl -s --noproxy '*' --anyauth -u root:"pass" -k --header "Content-Type: application/json" --request POST --data '{"apiVersion":"1.0","method":"getProperties", "params": {"propertyList": ["Architecture"]}}' http://172.25.70.46/axis-cgi/basicdeviceinfo.cgi)
-echo  "response from curl= $ok" 
-ok=${ok#*Architecture}
-echo "test=$(echo $ok | cut -d '"' -f 3)" >> $GITHUB_ENV
-echo ok=$(echo $ok | cut -d '"' -f 3)
-
-ok=$(curl -s --noproxy '*' --anyauth -u root:"pass" -k --header "Content-Type: application/json" --request POST --data '{"apiVersion":"1.0","method":"getProperties", "params": {"propertyList": ["Architecture"]}}' http://172.25.65.98/axis-cgi/basicdeviceinfo.cgi)
-echo  "response from curl= $ok" 
-ok=${ok#*Architecture}
-echo "test=$(echo $ok | cut -d '"' -f 3)" >> $GITHUB_ENV
-echo ok=$(echo $ok | cut -d '"' -f 3)
+for i in {1..5}; do
+    sleep 1
+    echo $i
+done
