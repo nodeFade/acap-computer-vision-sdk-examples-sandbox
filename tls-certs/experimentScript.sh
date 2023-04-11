0e72d71f9b659120ef6f9e71a04cbff3ff48eca7
nodeFade <Fadelb@axis.com>
2023-04-11 11:45:35 +0200

    fixing same source images for un-manupulating.

--- tls-certs/experimentScript.sh
+++ tls-certs/experimentScript.sh
@@ -6,7 +6,11 @@
 #     echo $i
 # done
 
+# alias "images"="docker images -a"
+# image: axisecp/acap-computer-vision-sdk:latest-aarch64-runtime
 
-curl -v -s --anyauth -u root:"pass" "http://172.25.65.98/axis-cgi/param.cgi?action=update&root.Network.SSH.Enabled=yes"
-ssh root@172.25.65.98 'command -v containerd >/dev/null 2>&1 && echo Compatible with Docker ACAP || echo Not compatible with Docker ACAP'
-curl -v -s --anyauth -u root:"pass" -k --header "Content-Type:application/json" http://172.25.65.98/axis-cgi/admin/systemlog.cgi?
+docker images -a
+docker image inspect --format='{{.RepoDigests}}' axisecp/acap-computer-vision-sdk:latest-aarch64-runtime
+docker save axisecp/acap-computer-vision-sdk:latest-aarch64-runtime -o  axisecp/acap-computer-vision-sdk:latest-aarch64-runtime.tar
+sha256sum axisecp/acap-computer-vision-sdk:latest-aarch64-runtime.tar
+docker rmi axisecp/acap-computer-vision-sdk:latest-aarch64-runtime.tar
