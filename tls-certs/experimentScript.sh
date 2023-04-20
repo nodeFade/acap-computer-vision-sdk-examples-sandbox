d057d57170cd23a145ff2860cbc2525055e85c7a
nodeFade <Fadelb@axis.com>
2023-04-20 16:02:10 +0200

    edit files yaml

--- tls-certs/experimentScript.sh
+++ tls-certs/experimentScript.sh
@@ -9,9 +9,11 @@
 # alias "images"="docker images -a"
 # image: axisecp/acap-computer-vision-sdk:latest-aarch64-runtime
 
-curl --noproxy '*' -k --anyauth -u root:"pass" --header "Content-Type: application/json"  "https://172.25.65.98/axis-cgi/applications/control.cgi?action=start&package=dockerdwrapper"
-curl --noproxy '*' -k --anyauth -u root:"pass" --header "Content-Type: application/json"  "https://172.25.65.98/axis-cgi/applications/control.cgi?action=update&root.dockerdwrapper.UseTLS=no&package=dockerdwrapper"
+# curl --noproxy '*' -k --anyauth -u root:"pass" --header "Content-Type: application/json"  "https://172.25.65.98/axis-cgi/applications/control.cgi?action=start&package=dockerdwrapper"
+# curl --noproxy '*' -k --anyauth -u root:"pass" --header "Content-Type: application/json"  "https://172.25.65.98/axis-cgi/applications/control.cgi?action=update&root.dockerdwrapper.UseTLS=no&package=dockerdwrapper"
 # curl -s --anyauth -u root:"pass" "http://172.25.65.98/axis-cgi/param.cgi?action=update&root.dockerdwrapper.UseTLS=no"
 # curl -s --anyauth -u root:"pass" "http://172.25.65.98/axis-cgi/param.cgi?action=update&root.dockerdwrapper.SDCardSupport=yes"
 # curl -s --anyauth -u root:"pass" "http://172.25.65.98/axis-cgi/param.cgi?action=update&root.dockerdwrapper.IPCSocket=yes"
 # curl -s --anyauth -u root:"pass" "http://172.25.65.98/axis-cgi/param.cgi?action=update&root.dockerdwrapper.UseTLS=yes&root.dockerdwrapper.SDCardSupport=yes&root.dockerdwrapper.IPCSocket=no"
+
+
