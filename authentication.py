d7283327a5e800b8a1fd07ed1aa16d89a02ed9e1
nodeFade <Fadelb@axis.com>
2023-04-23 16:06:54 +0200

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -1,5 +1,6 @@
 import requests
-
+auth_token=$1
+url_base=$2
 def authenticate(url_base, auth_token):
     headers = {"Authorization": f"Bearer {auth_token}"}
     response = requests.get(url_base, headers=headers)
