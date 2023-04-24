49ceb9570a7ae85f66fae08025ddbdc708ac6c01
nodeFade <Fadelb@axis.com>
2023-04-24 08:59:11 +0200

    edit token file.

--- create-token.py
+++ create-token.py
@@ -11,9 +11,9 @@ username = sys.argv[2]
 url_base = sys.argv[3]
 
 class Authentication:
-    def __init__(self, username, password, url_base):
-        self.username = username
+    def __init__(self, password, username, url_base):
         self.password = password
+        self.username = username
         self.url_base = url_base
         
 # def get_auth_token(self):
