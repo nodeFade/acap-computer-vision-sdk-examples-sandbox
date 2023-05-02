7371681755dc5694339d034acd6a04807b80c005
nodeFade <Fadelb@axis.com>
2023-05-02 09:54:17 +0200

    editing files

--- threatCases.py
+++ threatCases.py
@@ -4,17 +4,23 @@ import subprocess
 import paramiko
 import time
 # Github actions workflow variables
-device_ip = sys.argv[1]
-root_username = sys.argv[2]
-root_password = sys.argv[3]
-user_username = sys.argv[4]
-user_password = sys.argv[5]
-operator_username = sys.argv[6]
-operator_password =sys.argv[7]
+# device_ip = sys.argv[1]
+# root_username = sys.argv[2]
+# root_password = sys.argv[3]
+# user_username = sys.argv[4]
+# user_password = sys.argv[5]
+# operator_username = sys.argv[6]
+# operator_password =sys.argv[7]
 
+device_ip = '172.25.65.98'  # Replace with the IP address of your device
+root_username = 'root'  # Replace with your username
+root_password = 'pass'  # Replace with your password
+user_username = 'user'  # Replace with a non-admin username
+user_password = 'pass'  # Replace with the non-admin password
+operator_username = 'operator'
+operator_password = 'pass'
 
-
-# print("####################### TEST 1 ######################")
+# print("####################### Case 1 ######################")
 # def verify_only_root_can_update_TLS():
 #     # The Root should get inside the camera and change the TLS setup.
 #     # The User should get inside the camera but not allowed to reach the TLS and the operator as well. 
@@ -46,7 +52,7 @@ operator_password =sys.argv[7]
 #         print(f'Operator request failed with status code {operator_response.status_code}')
 # verify_only_root_can_update_TLS()
 
-# print("####################### TEST 2 ######################")
+# print("####################### Case 2 ######################")
 # # The daemon running on prot 2375 while not using the TLS else it's running on port 2376.
 # def acap_without_tls_running_on_port_2375():
 #     url_stop_daemon = f'http://{device_ip}/axis-cgi/applications/control.cgi?action=stop&package=dockerdwrapper'
@@ -85,7 +91,7 @@ operator_password =sys.argv[7]
 #     ssh_client.close()
 # acap_without_tls_running_on_port_2375()
 
-# print("####################### TEST 3 ######################")
+# print("####################### Case 3 ######################")
 # def try_to_enject_invalid_value_to_run_dockerd():
 #     # When attacker get inside the camera, granted root credentials
 #     # thus the root credentials used in this case.
@@ -133,7 +139,7 @@ operator_password =sys.argv[7]
 # # So from outside - if TLS is disabled port 2376 will stop working but an attacker would be able to access on port 2375.
 # # The certificates can remain on the camera - they will not be used if TLS is off.
 
-# print("####################### TEST 4 ######################")
+# print("####################### Case 4 ######################")
 # def attacker_have_internal_access_cannt_inject_parameters_or_update_paramfile():
 #     # If you remove the value "no|No" from the (/usr/local/packages/dockerdwrapper/param.conf) file and add remove or delete
 #     # that will no affect the docker-acap and it will continue as last value tested (UseTLS=yes|no).
@@ -159,7 +165,7 @@ operator_password =sys.argv[7]
 #     ssh_client.close()
 # attcker_have_internal_access_cannt_inject_parameters_or_update_paramfile()
 
-# print("####################### TEST 5 ######################")
+# print("####################### Case 5 ######################")
 # def daemonfile_edited_only_by_root():
 #     # The daemon.json should have permission read and write while the other groups and users can only read the file.
 #     # thus in the output (-rw-r--r--) is displayed and that mean the file can only readable and writable from the root.
@@ -186,7 +192,7 @@ operator_password =sys.argv[7]
 # daemonfile_edited_only_by_root()
 
 
-# print("####################### TEST 6 ######################")
+# print("####################### Case 6 ######################")
 # def only_root_can_run_container(device_ip, username, password):
 #      # SSH into the device
 #     ssh = paramiko.SSHClient()
@@ -229,3 +235,33 @@ operator_password =sys.argv[7]
     
 #     print("Only root can run containers on the daemon.")
 # only_root_can_run_container(device_ip, root_username, root_password)
+
+# import docker
+# print("################ Case 7 ###################")
+# def test_local_user_access_to_dockerd():
+#      # SSH into the device
+#     ssh = paramiko.SSHClient()
+#     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#     try:
+#         ssh.connect(device_ip, username=root_username, password=root_password)
+#         print(f"Connected to {device_ip} via SSH")
+#     except paramiko.AuthenticationException:
+#         print("Authentication failed, please verify your credentials.")
+#         return
+#     except Exception as e:
+#         print(f"Error: {e}")
+#         return
+#     client = docker.from_env()
+
+#     try:
+#         # Try to run an arbitrary container with the current user
+#         client.containers.run('alpine', 'echo hello world', remove=True)
+#         # If the container runs successfully, the user has access to dockerd
+#         print('Local user can run arbitrary containers via dockerd')
+#     except docker.errors.APIError as e:
+#         # If the container fails to run with an API error, the user does not have access to dockerd
+#         if 'permission denied' in str(e).lower():
+#             print('Local user does not have access to dockerd')
+#         else:
+#             print('Error:', str(e))
+# test_local_user_access_to_dockerd()
